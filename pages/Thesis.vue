4112a193af82b430038719b163e4277ceb00f612
georgesoteriou <george@soteriou.me>
2022-08-09 12:21:26 +0200

    thesis

---
+++ pages/Thesis.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout ma-5 row justify-center align-center>
+    <vue-pdf-embed
+      :source="T"
+      style="width: 80%; margin: 20px auto"
+    ></vue-pdf-embed>
+  </v-layout>
+</template>
+
+<script>
+import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed'
+
+export default {
+  data() {
+    return {
+      T: '/docs/thesis.pdf',
+    }
+  },
+  components: {
+    VuePdfEmbed,
+  },
+}
+</script>
+
+<style>
+.vue-pdf-embed > div {
+  margin-bottom: 20px;
+}
+</style>
