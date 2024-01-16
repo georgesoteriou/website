3ba5f24cfcdf50e3aff099065850c93f0809c21c
George Soteriou <george@soteriou.me>
2024-01-16 17:11:10 +0000

    Add banana

---
+++ pages/Banana.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12>
+        alert("b" + "a" + +"a" + "a")
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import * as GitgraphJS from '@gitgraph/js/lib'
+
+export default {
+  mounted() {
+    alert("b" + "a" + +"a" + "a")
+  }
+}
+</script>
